1ec7b5fc39727749d04ad392fd3db5ba867238f6
rbneiman <alecneiman@gmail.com>
2023-03-03 17:05:22 -0600

    optimization

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import serial
 import imageio
 import numpy as np
 
+SQUARE_LEN = 16
 
 # https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
 def blockshaped(arr, nrows, ncols):
@@ -26,18 +27,23 @@ def image_to_squares(r, g, b):
     g = (g.astype(np.uint16) & 0xFC) << 3
     b = (b.astype(np.uint16) & 0xF8) >> 3
     rgb565 = r | g | b
-    img_rows = blockshaped(rgb565, 8, 8)
+    img_rows = blockshaped(rgb565, SQUARE_LEN, SQUARE_LEN)
     squares = []
     for square in img_rows:
-        squares.append(square.flatten().tobytes())
+        sp = []
+        flattened = square.flatten()
+        num_arrs = (flattened.shape[0]*2) // 256
+        for arr in np.split(flattened, num_arrs):
+            sp.append(arr.tobytes())
+        squares.append(sp)
+
     return squares
 
 
-def stream_to_device(dev: str, image: str, baud=9600):
+def stream_to_device(dev: str, image: str, baud=115200):
     img = imageio.imopen(image, "r")
     img_data = img.read()
     img_meta = img.metadata()
-    # Use a breakpoint in the code line below to debug your script.
 
     if 'palette' in img_meta:
         palette = img_meta['palette']
@@ -70,23 +76,23 @@ def stream_to_device(dev: str, image: str, baud=9600):
         # ser.read_until(expected=b'\xFF')
         cmd = ser.read()
         if cmd == b'\x01':
-            print("\nHOST: return num images")
+            print(f"\nHOST: return num squares: {len(squares)}")
             # write_verified(ser, len(squares).to_bytes(2, byteorder="little", signed=False))
             ser.write(len(squares).to_bytes(2, byteorder="little", signed=False))
         elif cmd == b'\x02':
             img_num = int.from_bytes(ser.read(2), byteorder="little", signed=False)
             print(f"HOST: return data from square: {img_num}")
             data = squares[img_num]
-            square_y = (img_num // 16) * 8
-            square_x = (img_num % 16) * 8
+            square_y = (img_num // (128 // SQUARE_LEN)) * SQUARE_LEN
+            square_x = (img_num % (128 // SQUARE_LEN)) * SQUARE_LEN
             ser.write(square_x.to_bytes(1, byteorder="little", signed=False))
             ser.read_until(b'\xAA')
             ser.write(square_y.to_bytes(1, byteorder="little", signed=False))
             ser.read_until(b'\xAA')
-            for i in range(0, len(data), 32):
-                for j in range(32):
-                    ser.write(data[i + j].to_bytes(1, byteorder="little", signed=False))
-                ser.read_until(b'\xAA')
+            for packet in data:
+                ser.write(packet)
+                # for j in range(256):
+
             print("HOST: Square transfer complete")
         elif cmd == b'\x03':
             active = False
@@ -95,4 +101,4 @@ def stream_to_device(dev: str, image: str, baud=9600):
 
 
 if __name__ == '__main__':
-    stream_to_device("COM5", "./rusty.png")
+    stream_to_device("COM5", "./mc.png")
